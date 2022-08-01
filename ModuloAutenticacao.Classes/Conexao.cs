b0de904390ff8b532c5fc0d3ef25250c3807fae6
CleitsonLima <cleitson.ferreira@aluno.senai.br>
2022-08-01 11:59:33 -0300

    iniciando dbconnetion

--- ModuloAutenticacao.Classes/Conexao.cs
+++ ModuloAutenticacao.Classes/Conexao.cs
@@ -1,10 +1,13 @@
 ﻿using System.Data.Common;
+using System.Data.SqlClient;
 
 namespace ModuloAutenticacao.Classes
 {
     internal class Conexao
     {
         public static DbConnection _conn;
+
+        public static SqlConnection MinhaInstancia { get; set; }
 
     }
 }
